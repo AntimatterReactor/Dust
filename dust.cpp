d13cddbe946903cece03b0bbdf83dd324d0cf95b
Ezra Alvarion <ezraalvarion71@gmail.com>
2023-12-29 14:27:39 +0700

    style: ran clang-format

--- dust.cpp
+++ dust.cpp
@@ -37,10 +37,7 @@
 
 #include "ds_utils.hpp"
 
-static std::vector<Token> tokens = {};
-static char next = 0;
-
-std::vector<Token> Lex(const char *fileName) // TODO: convert to filepath
+std::vector<Token> Lex(const char *fileName)
 {
     std::ifstream fp(fileName);
     char c = 0, p = 0;
@@ -48,7 +45,7 @@ std::vector<Token> Lex(const char *fileName) // TODO: convert to filepath
     std::vector<Token> r;
     do {
         fp.get(c);
-        if (!isvalidBF(c)) continue;
+        if (!isvalidBF(c) && fp.good()) continue;
 
         switch (p) {
         case '>':
@@ -92,34 +89,50 @@ std::vector<Token> Lex(const char *fileName) // TODO: convert to filepath
     return r;
 }
 
+// Optimizes very common simple brainfuck algorithms
 std::vector<Token> Optimize(const std::vector<Token> &v)
 {
-    std::vector<Token> r;
+    std::vector<Token> r1, r2;
     bool hc = false;
     for (size_t i = 0; i < v.size(); ++i) {
         // Subtitutes `[-]` for 'e' instruction
         if (i < v.size() - 3 && v.at(i).c == '[' &&
             v.at(i + 1) == Token{'n', -1} && v.at(i + 2).c == ']') {
             // Prevents generation of 'e' inst before any 'n' insts are made
-            if(hc) r.push_back({'e', 1});
+            if (hc) r1.push_back({'e', 1});
             i += 2;
         }
-        else r.push_back(v.at(i));
-        if (v.at(i).c == 'n') hc = true;
+        else r1.push_back(v.at(i));
+        if (v.at(i).c == 'n' || v.at(i).c == ',') hc = true;
     }
-    return r;
+    return r1;
+    // TODO: implement a second pass to merge the previously disjointed
+    //* (by the no longer existing 'e') instructs
+    // for (size_t i = 0; i < r1.size(); ++i) {
+    //     if (i > 0 && r1.at(i).c == r1.at(i - 1).c)
+    //     {
+    //         while () {}
+    //         r2.push_back({r1.at(i).c, r1.at(i).amount + r1.at(i -
+    //         1).amount});
+    //     }
+    //     else r2.push_back(r1.at(i));
+    // }
+    // return r2;
+    // TODO: implement 'a' inst for absolute movement
 }
 
 class AST {
-public:
+    public:
     AST() {}
     virtual ~AST();
     virtual llvm::Value *codegen() = 0;
     virtual void spew() = 0;
 };
 
-AST::~AST() {} //! DO NOT REMOVE, this is used so that no linker vtable error occur
+AST::~AST() {
+} //! DO NOT REMOVE, this is used so that no linker vtable error occur
 
+//
 class ExprAST : public AST {
     std::vector<Token> tok;
 
@@ -140,13 +153,15 @@ class StmtAST : public AST {
     std::optional<std::vector<std::unique_ptr<AST>>> children;
 
     public:
-    StmtAST(std::vector<std::unique_ptr<AST>> &&vptr) : children(std::move(vptr)) {}
+    StmtAST(std::vector<std::unique_ptr<AST>> &&vptr)
+        : children(std::move(vptr))
+    {
+    }
 
     llvm::Value *codegen() override;
     void spew() override
     {
-        if (this->children)
-        {
+        if (this->children) {
             std::cout << "{{{\n";
             for (auto &n : this->children.value())
                 n->spew();
@@ -163,10 +178,8 @@ std::unique_ptr<AST> ParseExpression(std::vector<Token>::const_iterator &nit)
     std::vector<Token> t;
     while (nit->c != '[' && nit->c != ']' && nit < tokens.cend())
         t.push_back(*(nit++));
-    if (t.size() == 0)
-        return nullptr;
-    if (nit == tokens.cend())
-        t.push_back(Token::None);
+    if (t.size() == 0) return nullptr;
+    if (nit == tokens.cend()) t.push_back(Token::None);
     return std::make_unique<ExprAST>(std::move(t));
 }
 
@@ -177,8 +190,7 @@ std::unique_ptr<AST> ParseStatement(std::vector<Token>::const_iterator &nit)
         if (auto e = ParseExpression(nit); e != nullptr)
             r.push_back(std::move(e));
         if (nit->c == '[') r.push_back(std::move(ParseStatement(++nit)));
-        if (nit->c == ']')
-        {
+        if (nit->c == ']') {
             ++nit;
             return std::make_unique<StmtAST>(std::move(r));
         }
@@ -186,14 +198,14 @@ std::unique_ptr<AST> ParseStatement(std::vector<Token>::const_iterator &nit)
     return std::make_unique<StmtAST>(std::move(r));
 }
 
-std::unique_ptr<AST> ParseTopStatement(std::vector<Token>::const_iterator &nit)
+std::unique_ptr<StmtAST>
+ParseTopStatement(std::vector<Token>::const_iterator &nit)
 {
     std::vector<std::unique_ptr<AST>> r;
     do {
         if (auto e = ParseExpression(nit); e != nullptr)
             r.push_back(std::move(e));
-        if (nit->c == '[')
-        {
+        if (nit->c == '[') {
             r.push_back(std::move(ParseStatement(++nit)));
             --nit;
         }
